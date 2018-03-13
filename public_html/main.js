fbd4e9f7b20298ddbc7e93d4f8fe5d6a59165441
Raj Pankajkumar Patel <000744572@mohawkcollege.ca>
2018-03-13 18:53:44 -0400

    added comments

--- public_html/main.js
+++ public_html/main.js
@@ -6,22 +6,30 @@ var map;
 var marker = new google.maps.Marker();
 //initialize the map with the user's location
 /**
- * 
+ * Methods is called at the end to put the map on the screen
  * @returns {undefined}
  */
 function intMap() {
+    //diclaring essential objects to make the user task work
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var geocoder = new google.maps.Geocoder;
     var infowindow = new google.maps.InfoWindow;
     var userAddress = "";
+    //setting the default longitude and latitude
     var center = {lat: 43.2362657216, lng: -79.8847431277};
+    //initializing map
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: center
     });
-    
+    //setting the map oon the screen
     directionsDisplay.setMap(map);
+
+    /**
+     * function to set the marker of the location user want's to go and the map is updated to show the traffic
+     * @returns {undefined}
+     */
     var change = function () {
         for (var a = 0; a < markerList.length; a++) {
             markerList[a].setMap(null);
@@ -33,7 +41,10 @@ function intMap() {
         getRoute(directionsService, directionsDisplay, mode);
     };
 
+    //each time the button is hit the change method is called to re-initialize map
     document.getElementById("getDirections").addEventListener('click', change);
+
+    //getting user location and putting it on the map
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
@@ -41,7 +52,7 @@ function intMap() {
                 lng: position.coords.longitude
             };
 
-            /////getting the address from the coordinates
+            //getting the address from the coordinates/current location of the user using geocoder
             geocoder.geocode({'location': pos}, function (results, status) {
                 if (status === 'OK') {
                     if (results[0]) {
@@ -56,6 +67,7 @@ function intMap() {
                         infowindow.open(map, marker);
                         marker.setAnimation(google.maps.Animation.DROP);
                         marker.setMap(map);
+                        //adding options to start from the location
                         $("#end").append('<option value="Mohawk College">Mohawk College</option>');
                         $("#end").append('<option id="current" value="' + userAddress + '">Your Location</option>');
 
@@ -67,16 +79,16 @@ function intMap() {
                 }
             });
 
-
-
+            //getting the values from the json object and putting markers at the longitude and latitude
             for (var i = 0; i < listLibrary.length; i++) {
+                //setting the longitude and latitude
                 var libPos = {lat: listLibrary[i].LATITUDE, lng: listLibrary[i].LONGITUDE};
-                //console.log(libPos);
+                //putting it on map and adding it to a list
                 markerList.push(new google.maps.Marker({
                     position: libPos
                 }));
                 markerList[i].setMap(map);
-                //console.log(listLibrary[i].NAME);
+                //adding the names of library into a div tag so user can refer it
                 $("#start").append('<option value="' + listLibrary[i].NAME + ' Hamilton">' + listLibrary[i].NAME + '</option>');
                 if (i <= 7)
                     $("#names1").append("<p>" + listLibrary[i].NAME + "</p>");
@@ -90,7 +102,7 @@ function intMap() {
 }
 
 /**
- * 
+ * Putting the destination in the map and selecting the route
  * @param {type} directionsService
  * @param {type} directionsDisplay
  * @param {type} mode
@@ -111,23 +123,24 @@ function getRoute(directionsService, directionsDisplay, mode) {
 }
 
 /**
- * 
+ * Reading the library name and finding it on map
  * @returns {undefined}
  */
 function getLibrary() {
     var name = document.getElementById("libName").value;
     name = name.toLowerCase();
+    name = name.trim();
     var infowindow = new google.maps.InfoWindow;
     var geocoder = new google.maps.Geocoder;
-
-    //console.log(name);
+    var flag = false;
     for (var i = 0; i < listLibrary.length; i++) {
         var lib = listLibrary[i].NAME.toLowerCase();
         if (lib === name) {
             if (markerList1.length > 0) {
                 marker.setMap(null);
             }
-            console.log(name);
+            $("#123").removeClass("has-error has-feedback");
+            $("#123").addClass("has-success has-feedback");
             for (var a = 0; a < markerList.length; a++) {
                 markerList[a].setMap(null);
             }
@@ -148,7 +161,12 @@ function getLibrary() {
                     }
                 }
             });
-
+            flag = true;
         }
+    }
+    if (!flag) {
+        $("#123").addClass("has-error has-feedback");
+        $("#123").addClass("has-success has-feedback");
+
     }
 }
